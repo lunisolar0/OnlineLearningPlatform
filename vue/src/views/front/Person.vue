63ae6bc31d58a2d8561c19161ab54a9b4790207e
lunisolar0 <1366036220@qq.com>
2024-11-05 16:54:38 +0800

    前端修正

--- vue/src/views/front/Person.vue
+++ vue/src/views/front/Person.vue
@@ -4,7 +4,7 @@
       <div style="text-align: right; margin-bottom: 20px">
         <el-button type="primary" @click="updatePassword">修改密码</el-button>
       </div>
-      <el-form :model="user" label-width="80px" style="padding-right: 20px">
+      <el-form :model="user" :rules="rules" ref="formRef" label-width="80px" style="padding-right: 20px">
         <div style="margin: 15px; text-align: center">
           <el-upload
               class="avatar-uploader"
@@ -34,7 +34,7 @@
       </el-form>
     </el-card>
     <el-dialog title="修改密码" :visible.sync="dialogVisible" width="30%" :close-on-click-modal="false" destroy-on-close>
-      <el-form :model="user" label-width="80px" style="padding-right: 20px" :rules="rules" ref="formRef">
+      <el-form :model="user" label-width="80px" style="padding-right: 20px" :rules="rules" ref="passwordFormRef">
         <el-form-item label="原始密码" prop="password">
           <el-input show-password v-model="user.password" placeholder="原始密码"></el-input>
         </el-form-item>
@@ -46,7 +46,7 @@
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="fromVisible = false">取 消</el-button>
+        <el-button @click="dialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="save">确 定</el-button>
       </div>
     </el-dialog>
@@ -65,11 +65,54 @@ export default {
         callback()
       }
     }
+
+    const validateName = (rule, value, callback) => {
+      const nameRegex = /^[\u4e00-\u9fa5]{2,4}$/;
+      if (!value) {
+        callback(new Error('请输入姓名'))
+      } else if (!nameRegex.test(value)) {
+        callback(new Error('姓名必须为2-4个汉字'))
+      } else {
+        callback()
+      }
+    }
+
+    const validatePhone = (rule, value, callback) => {
+      const phoneRegex = /^1\d{10}$/;
+      if (!value) {
+        callback(new Error('请输入手机号'))
+      } else if (!phoneRegex.test(value)) {
+        callback(new Error('手机号须为1开头的11位数字串'))
+      } else {
+        callback()
+      }
+    }
+
+    const validateEmail = (rule, value, callback) => {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!value) {
+        callback(new Error('请输入邮箱'))
+      } else if (!emailRegex.test(value)) {
+        callback(new Error('邮箱格式不正确'))
+      } else {
+        callback()
+      }
+    }
+
     return {
       user: JSON.parse(localStorage.getItem('xm-user') || '{}'),
       dialogVisible: false,
 
       rules: {
+        name: [
+          { validator: validateName, required: true, trigger: 'blur' },
+        ],
+        phone: [
+          { validator: validatePhone, required: true, trigger: 'blur' },
+        ],
+        email: [
+          { validator: validateEmail, required: true, trigger: 'blur' },
+        ],
         password: [
           { required: true, message: '请输入原始密码', trigger: 'blur' },
         ],
@@ -82,40 +125,33 @@ export default {
       }
     }
   },
-  created() {
-
-  },
   methods: {
     update() {
-      // 保存当前的用户信息到数据库
-      this.$request.put('/user/update', this.user).then(res => {
-        if (res.code === '200') {
-          // 成功更新
-          this.$message.success('保存成功')
-          // 更新浏览器缓存里的用户信息
-          localStorage.setItem('xm-user', JSON.stringify(this.user))
-
-          // 触发父级的数据更新
-          this.$emit('update:user')
-        } else {
-          this.$message.error(res.msg)
+      this.$refs.formRef.validate((valid) => {
+        if (valid) {
+          this.$request.put('/user/update', this.user).then(res => {
+            if (res.code === '200') {
+              this.$message.success('保存成功')
+              localStorage.setItem('xm-user', JSON.stringify(this.user))
+              this.$emit('update:user')
+            } else {
+              this.$message.error(res.msg)
+            }
+          })
         }
       })
     },
     handleAvatarSuccess(response, file, fileList) {
-      // 把user的头像属性换成上传的图片的链接
       this.$set(this.user, 'avatar', response.data)
     },
-    // 修改密码
     updatePassword() {
       this.dialogVisible = true
     },
     save() {
-      this.$refs.formRef.validate((valid) => {
+      this.$refs.passwordFormRef.validate((valid) => {
         if (valid) {
           this.$request.put('/updatePassword', this.user).then(res => {
             if (res.code === '200') {
-              // 成功更新
               this.$message.success('修改密码成功')
               this.$router.push('/login')
             } else {
@@ -128,6 +164,7 @@ export default {
   }
 }
 </script>
+
 
 <style scoped>
 /deep/.el-form-item__label {
